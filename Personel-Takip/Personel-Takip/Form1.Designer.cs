5134ab7c63e1f3d19a9798a30e3e1f2a8045916f
Erol Dalkılıç <eroldalg@gmail.com>
2022-08-29 23:07:39 +0300

    finish

--- Personel-Takip/Personel-Takip/Form1.Designer.cs
+++ Personel-Takip/Personel-Takip/Form1.Designer.cs
@@ -151,6 +151,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(17, 42);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(207, 232);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 14;
             this.pictureBox1.TabStop = false;
             // 
--- Personel-Takip/Personel-Takip/Form1.cs
+++ Personel-Takip/Personel-Takip/Form1.cs
@@ -9,11 +9,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.IO.Ports;
+using System.Data.OleDb;
 
 namespace Personel_Takip
 {
     public partial class Form1 : Form
     {
+
+        OleDbConnection bag = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Personel-Veri.accdb");
+        OleDbCommand cmd = new OleDbCommand();
+
         public static string portismi, banthizi;
         string[] ports = SerialPort.GetPortNames();
 
@@ -100,6 +105,40 @@ namespace Personel_Takip
             if (sonuc != "")
             {
                 label2.Text = sonuc;
+                bag.Open();
+
+                cmd.Connection = bag;
+                cmd.CommandText="Select * FROM tablo WHERE Kid='" + sonuc + "'";
+
+                OleDbDataReader reader = cmd.ExecuteReader();   
+
+                if(reader.Read())
+                {
+                    DateTime bugun = DateTime.Now;
+                    pictureBox1.Image = Image.FromFile("foto\\"+reader["resim"].ToString());
+                    label8.Text = reader["isim"].ToString();
+                    label9.Text = reader["sinif"].ToString() + "/" + reader["sube"].ToString();
+                    label10.Text=bugun.ToShortDateString();
+                    label11.Text = bugun.ToLongTimeString();
+                    bag.Close();
+
+                    bag.Open();
+                    cmd.CommandText = "INSERT INTO zaman (isim,tarih,saat)VALUES ('" + label8.Text + "','" + label10.Text + "','" + label11.Text + "')";
+                    cmd.ExecuteNonQuery();
+                    bag.Close();
+
+                }
+                else
+                {
+                    pictureBox1.Image = Image.FromFile("foto\\images.png" );
+                    label2.Text = "Kart Kayıtlı Değil";
+                    label8.Text = "_____________";
+                    label9.Text = "_____________";
+                    label10.Text = "_____________";
+                    label11.Text = "_____________";
+                }
+
+                bag.Close();
             }
         }
         private void button3_Click(object sender, EventArgs e)
